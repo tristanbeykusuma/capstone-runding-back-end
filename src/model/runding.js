41635ca0a4bb88f6f7c58acc58557928460d4365
tristanbeykusuma <tristanbeykusuma@gmail.com>
2022-11-25 05:34:33 +0800

    Fix routing issue, add email to user data, and seperate routes and route handlers in seperate module middleware

--- src/model/runding.js
+++ src/model/runding.js
@@ -6,6 +6,7 @@ const RundingSchema = new mongoose.Schema(
     logo_grup: { type: String, required: true },
     subject: { type: String, required: true },
     deskripsi: { type: String, required: true },
+    administrator: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserSchema" }],
     peserta: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserSchema" }],
   },
   { collection: "rundings" }
