c7d6709f891d1bd99cc4e1732f9b16d72864774e
triadiningsih <triadiningsih65@gmail.com>
2022-11-29 11:36:32 +0800

    Create replies model

---
+++ src/model/replies.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+//skema data dari database mongodb (dibutuhkan untuk melakukan crud dengan mongoose)
+const RepliesSchema = new mongoose.Schema(
+  {
+    comment_id: { type: mongoose.Schema.Types.ObjectId, ref: "CommentSchema" },
+    content: { type: String, required: true },
+    likes: { type: [mongoose.Schema.Types.ObjectId], ref: "UserSchema", },
+    author_id: [{type: mongoose.Schema.Types.ObjectId, ref: "UserSchema"}],
+    author_username: [{type: String}],
+  },
+  { collection: "Replies", timestamps: true }
+);
+
+const Replies = mongoose.model("RepliesSchema", RepliesSchema);
+
+module.exports = Replies;
